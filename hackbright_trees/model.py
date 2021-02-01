fa1cfd05d2238bd0bdaebcbee599a8d288f8d122
alexpsimone <alex.p.simone@gmail.com>
2021-02-01 02:20:20 +0000

    added dfs method

--- hackbright_trees/model.py
+++ hackbright_trees/model.py
@@ -6,7 +6,28 @@ class Node():
         self.data = data
         self.children = children or []
 
+
     def __repr__(self):
         """User-friendly representation"""
 
         return f'<Node {self.data}>'
+
+
+    def dfs(self, data):
+        """Return node object with the given data."""
+
+        to_visit = [self]
+
+        while to_visit:
+            current = to_visit.pop()
+
+            if current.data == data:
+                return current
+            
+            to_visit.extend(current.children)
+    
+
+    def recursive_dfs(self, data):
+        """Search for Node object with given data, recursively."""
+
+        pass

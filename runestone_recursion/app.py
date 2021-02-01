221b3907c5a45d680f5453566e2cee718a437b3d
alexpsimone <alex.p.simone@gmail.com>
2021-02-01 03:23:22 +0000

    factorial, reverse list funcs

--- runestone_recursion/app.py
+++ runestone_recursion/app.py
@@ -1,5 +1,36 @@
-def reverse_factorial(num):
-    """Determine the factorial of a number, recursively."""
+def recursive_factorial(num):
+    """Determine the factorial of a number, recursively.
     
-def reverse_list(test_list):
-    """Reverse a list, recursively."""
+    >>> recursive_factorial(3)
+    6
+    >>> recursive_factorial(0)
+    1
+
+    """
+
+    if num == 0:
+        return 1
+    
+    if num == 1: 
+        return num
+    
+    else:
+        return num * recursive_factorial(num - 1)
+
+
+def reverse_list(lst):
+    """Reverse a list, recursively.
+    
+    >>> reverse_list([])
+    []
+    >>> reverse_list([1])
+    [1]
+    >>> reverse_list([1,2,3])
+    [3, 2, 1]
+    """
+
+    if len(lst) < 2:
+        return lst
+    
+    else:
+        return [lst[-1]] + reverse_list(lst[:(len(lst) - 1)])

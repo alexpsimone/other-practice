04131d4dc2600dd90bf7ab3ba4d20c3fe4cb3168
alexpsimone <alex.p.simone@gmail.com>
2021-02-01 02:11:39 +0000

    files moved

--- hackbright_trees/model.py
+++ hackbright_trees/model.py
@@ -2,9 +2,9 @@
 class Node():
     """A node object in a tree."""
 
-    def __init__(self, data, children):
+    def __init__(self, data, children=None):
         self.data = data
-        self.children = children
+        self.children = children or []
 
     def __repr__(self):
         """User-friendly representation"""

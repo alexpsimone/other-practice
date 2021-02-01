0b84f577f298cc5c07b08a70bcf3b6aeef212150
alexpsimone <alex.p.simone@gmail.com>
2021-02-01 03:33:16 +0000

    initial commit

---
+++ runestone_recursion/recursive_tree.py
@@ -0,0 +1,27 @@
+# Practice code, copied from Runestone Academy (linked below) and modified.
+# https://runestone.academy/runestone/books/published/pythonds/Recursion/pythondsintro-VisualizingRecursion.html?highlight=recursive%20tree
+
+import turtle
+
+def tree(branchLen,t):
+    if branchLen > 5:
+        t.forward(branchLen)
+        t.right(20)
+        tree(branchLen-15,t)
+        t.left(40)
+        tree(branchLen-15,t)
+        t.right(20)
+        t.backward(branchLen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.color("green")
+    tree(75,t)
+    myWin.exitonclick()
+
+main()
